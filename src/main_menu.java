c91165673a52ff9e3404e301c14930807493764f
Michael P. Hebner <hebner211@gmail.com>
2020-04-14 13:47:39 -0400

    Need to finish commenting and clean up

--- src/main_menu.java
+++ src/main_menu.java
@@ -7,6 +7,7 @@ public class main_menu {
     ArrayList<String> operators = new ArrayList<>();
 
     public main_menu(){
+        //initialize input as empty string
         input = "";
     }
 
@@ -15,6 +16,7 @@ public class main_menu {
         System.out.println("    [1] Read in expression in postfix notation");
         System.out.println("    [2] Evaluate the postfix expression");
         System.out.println("    [3] CLEAR");
+        System.out.println("    [4] CHECK CLEAR");
         System.out.println("    [0] EXIT");
     }
 
@@ -30,8 +32,13 @@ public class main_menu {
     }
 
     public void stackIntegers(){
+        //splits elements by space and stores them as variable x
         var x = input.split(" ");
         for(var elm:x){
+            //this looks for any operator
+            //I could only get it to work by using this regex
+            //Only using the four allowed operators did not work
+            //If an operator is found, it's stored in the arraylist of operators
             if(elm.matches("[()=;{}[]+\\-*/&!%^|<>']]")){
                 operators.add(elm);
             }
@@ -42,17 +49,23 @@ public class main_menu {
             }
         }
         theStack.displayStack();
-        System.out.print("\n"+ operators+"\n");
+        System.out.print("Operators, listed in order:"+operators+"\n");
+
     }
 
     public void evalPostfix(){
+        //loop will cycle as long as there is operators left to perform work with
         while(operators.size()>0){
             int x = (int) theStack.pop();
             int y = (int) theStack.pop();
             var q = operators.get(0);
+            //result variable is used to temporarily store the outcome from the arithmetic
             var result = 0;
+            //operator can now be removed from array list since it is temporarily stored as variable q
             operators.remove(0);
             System.out.print(y+" "+ q +" "+ x +"\n");
+            //Switch statement is used to decode operator that is currently stored as a string
+            //and to preform the math
             switch(q){
                 case "+":
                     result = y+x;
@@ -69,6 +82,7 @@ public class main_menu {
                 default:
                     System.out.print("INVALID INPUT");
             }
+            //result is then pushed back onto the stack
             theStack.push(result);
             System.out.print("="+ result+"\n");
 
@@ -92,11 +106,13 @@ public class main_menu {
                     evalPostfix();
                     break;
                 case 3:
+                    //I implemented this clear option to ensure the stack is emptied before an other run
                     while (!theStack.isEmpty()){
                         theStack.pop();
                     }
                     break;
                 case 4:
+                    //This is to visually confirm the stack has been cleared after clearing
                     theStack.displayStack();
                     break;
                 case 0:
